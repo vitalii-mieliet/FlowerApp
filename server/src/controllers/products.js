c6fe308027b0fd28ae6732f01021c6197bbfe6e0
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-09-10 21:39:44 +0300

    feat: feat(products): add getProducts service with shop filtering

--- server/src/controllers/products.js
+++ server/src/controllers/products.js
@@ -1,7 +1,14 @@
+import createHttpError from 'http-errors';
+import { isValidObjectId } from 'mongoose';
 import { getProducts } from '../services/products.js';
 
 export const getProductsController = async (req, res) => {
-  const data = await getProducts();
+  const { shopId } = req.query;
+
+  if (!isValidObjectId(shopId))
+    throw createHttpError(400, `Shop ID: "${shopId}" is not valid!`);
+
+  const data = await getProducts(shopId);
 
   res.status(200).json({
     status: 200,
--- server/src/services/products.js
+++ server/src/services/products.js
@@ -1,6 +1,6 @@
 import Product from '../db/models/Product.js';
 
-export const getProducts = async () => {
-  const data = await Product.find();
+export const getProducts = async (shopId) => {
+  const data = await Product.find({ shopId });
   return data;
 };
